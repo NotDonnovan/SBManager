e83568cb7471c1d86452716a42a96dcab5e15840
NotDonnovan <dpascascio@gmail.com>
2020-10-25 21:23:14 -0500

    Added qbittorrent API

--- SBManager/settings.py
+++ SBManager/settings.py
@@ -123,6 +123,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    '/home/dpasc/PycharmProjects/SBManager/SBManager/static',
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- application/views.py
+++ application/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+import qbittorrentapi
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
+
+qbt_client = qbittorrentapi.Client(
+    host=env('qbt_host'),
+    port=8080,
+    username=env('qbt_user'),
+    password=env('qbt_password'))
+
 
 def home(request):
-    return render(request, 'application/index.html')
+
+    torrents = [{'tname': torrent.name, 'tstate': torrent.state}
+                 for torrent in qbt_client.torrents_info()]
+
+    return render(request, 'application/index.html', {'torrents': torrents})
