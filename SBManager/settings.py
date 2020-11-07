7eb57023181eaec2ed3565027e94755828a02add
NotDonnovan <dpascascio@gmail.com>
2020-11-07 11:14:34 -0600

    Finished categories

--- SBManager/settings.py
+++ SBManager/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    '/home/dpasc/PycharmProjects/SBManager/SBManager/static',
+    '/mnt/c/Users/Dpasc/Desktop/Donnovan/Projects/SBManager/SBManager/static',
 )
 
 
